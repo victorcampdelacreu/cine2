76309eb72468ea505b2cf6e0756b36c21df53968
victorcampdelacreu <85998204+victorcampdelacreu@users.noreply.github.com>
2021-07-05 12:05:12 +0200

    en_informacion

--- en_informacion.php
+++ en_informacion.php
@@ -5,31 +5,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informacion</title>
-    
+    <title>Administracion</title>
+    <!-- <link rel="stylesheet" href="estilos.css"> -->
     <link rel="stylesheet" href="estilos2.css">
 </head>
 
-<body id="body">
+<body class="body">
     <div class="cajasMenu-2">
         
             <h1 class="texto">               
-                    cines y cartelera<br>
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<br>
-                    yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy<br>
-                    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz<br>
-                    Todos los servicios estan disponibles las 24 horas del dia                
+                    En Cine puede encontrar la cartelera de la fecha que desee<br>
+                    Seleccione la opcion de sala, sesion y pelicula que desee<br>
+                    Elija los asientos (butacas) que quiera comprar<br>
+                    Confirme las butacas compradas o rechacelas si no quiere seguir<br>
+                    Imprima las entradas compradas                
             </h1>   
         
     </div>
-        <br>
-        <br>
-        <br>
-        <br>
-    <div class = "cajasMenu">
-        <div class="cajas">
+        
+        <
+    <!--<div class = "cajasMenu">-->
+        <div class="cajas2">
                 <h2>
                     <a class="enlaces" href="index.php">Ir al inicio</a>
                 </h2>
         </div>
     </div>
+
+
+</body>
+</html>
